fdd5dca5e61103142d4d9d2468fc44028ebfb257
EC2 Default User <ec2-user@staging.localgood.jp>
2017-10-03 14:59:54 +0900

    Change position of image

--- view/project/widget/rewards-summary.html.php
+++ view/project/widget/rewards-summary.html.php
@@ -54,6 +54,9 @@ uasort($project->individual_rewards,
         <?php foreach ($project->individual_rewards as $individual) :
             ?>
         <li class="<?php echo $individual->icon ?>">
+			<div class="image">
+				<img src="<?php echo $individual->image->getLink(580, 580) ?>" alt="<?/*php todo: $project->新しく追加されるお礼画像のキャプション　を出す */?>">
+			</div>
             <dl class="amount">
                 <dt><?php echo Text::get('regular-support-amount'); ?></dt>
                 <dd><strong><?php echo \amount_format($individual->amount); ?></strong>円</dd>
@@ -72,9 +75,6 @@ uasort($project->individual_rewards,
                 <div class="buttons">
                     <a class="button violet supportit" href="/project/<?php echo $project->id; ?>/invest"><?php echo Text::get('regular-invest'); ?></a>
                 </div>
-            </div>
-            <div class="right">
-                <img src="<?php echo $individual->image->getLink(580, 580) ?>" alt="<?/*php todo: $project->新しく追加されるお礼画像のキャプション　を出す */?>">
             </div>
 
         </li>
