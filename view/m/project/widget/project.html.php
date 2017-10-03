29133ece698712c0273f135c73084561e778ea9f
Naoko Usui <usui@info-lounge.jp>
2017-10-03 16:30:31 +0900

    Fix tagmark position, author name in project widget for SP

--- view/m/project/widget/project.html.php
+++ view/m/project/widget/project.html.php
@@ -57,28 +57,29 @@ if (isset($this['investor']) && is_object($this['investor'])) {
         <?php if (!empty($project->gallery) && (current($project->gallery) instanceof Image)): ?>
         <a href="<?php echo SITE_URL ?>/project/<?php echo $project->id ?>" target="_parent"><img alt="<?php echo $project->name ?>" src="<?php echo current($project->gallery)->getLink(500, 285, true) ?>" /></a>
         <?php endif ?>
+
+        <?php switch ( $project->tagmark ) {
+            case 'onrun': // "en marcha"
+                echo '<div class="tagmark green">' . Text::get( 'regular-onrun_mark' ) . '</div>';
+                break;
+            case 'keepiton': // "aun puedes"
+                echo '<div class="tagmark green">' . Text::get( 'regular-keepiton_mark' ) . '</div>';
+                break;
+            case 'onrun-keepiton': // "en marcha" y "aun puedes"
+                //                echo '<div class="tagmark green">' . Text::get('regular-onrun_mark') . '</div>';
+                echo '<div class="tagmark green twolines"><span class="small"><strong>' . Text::get( 'regular-onrun_mark' ) . '</strong><br />' . Text::get( 'regular-keepiton_mark' ) . '</span></div>';
+                break;
+            case 'gotit': // "financiado"
+                echo '<div class="tagmark violet">' . Text::get( 'regular-gotit_mark' ) . '</div>';
+                break;
+            case 'success': // "exitoso"
+                echo '<div class="tagmark red">' . Text::get( 'regular-success_mark' ) . '</div>';
+                break;
+            case 'fail': // "caducado"
+                echo '<div class="tagmark grey">' . Text::get( 'regular-fail_mark' ) . '</div>';
+                break;
+        } ?>
     </div>
-	<?php switch ( $project->tagmark ) {
-		case 'onrun': // "en marcha"
-			echo '<div class="tagmark green">' . Text::get( 'regular-onrun_mark' ) . '</div>';
-			break;
-		case 'keepiton': // "aun puedes"
-			echo '<div class="tagmark green">' . Text::get( 'regular-keepiton_mark' ) . '</div>';
-			break;
-		case 'onrun-keepiton': // "en marcha" y "aun puedes"
-			//                echo '<div class="tagmark green">' . Text::get('regular-onrun_mark') . '</div>';
-			echo '<div class="tagmark green twolines"><span class="small"><strong>' . Text::get( 'regular-onrun_mark' ) . '</strong><br />' . Text::get( 'regular-keepiton_mark' ) . '</span></div>';
-			break;
-		case 'gotit': // "financiado"
-			echo '<div class="tagmark violet">' . Text::get( 'regular-gotit_mark' ) . '</div>';
-			break;
-		case 'success': // "exitoso"
-			echo '<div class="tagmark red">' . Text::get( 'regular-success_mark' ) . '</div>';
-			break;
-		case 'fail': // "caducado"
-			echo '<div class="tagmark grey">' . Text::get( 'regular-fail_mark' ) . '</div>';
-			break;
-	} ?>
 
     <div class="project-details">
         <h<?php echo $level ?> class="title">
