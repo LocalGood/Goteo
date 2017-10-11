866b52355c116e2a12196bd6477a06f2928253d2
EC2 Default User <ec2-user@staging.localgood.jp>
2017-10-11 11:09:13 +0900

    Fix indent

--- view/dashboard/menu.html.php
+++ view/dashboard/menu.html.php
@@ -22,16 +22,16 @@
     <ul>
     <?php foreach ($this['menu'] as $section=>$item) : ?>
 		<?php if( !( $item['permission'] === false)):?>
-        <li class="section<?php if ($section == $this['section']) echo ' current'; if (($section == 'projects') && ($item['permission'] == false) ) echo ' forbidden'; ?>">
-            <a class="section" href="/dashboard/<?php echo $section; ?>"><?php echo $item['label']; ?></a>
-            <ul>
-            <?php foreach ($item['options'] as $option=>$label) : ?>
-                <li class="option<?php if ($section == $this['section'] && $option == $this['option']) echo ' current'; ?>">
-                    <a href="/dashboard/<?php echo $section; ?>/<?php echo $option; ?>"<?php if ($option == 'public') echo ' target="_blank"' ?>><?php echo $label; ?></a>
-                </li>
-            <?php endforeach; ?>
-            </ul>
-        </li>
+			<li class="section<?php if ($section == $this['section']) echo ' current'; if (($section == 'projects') && ($item['permission'] == false) ) echo ' forbidden'; ?>">
+				<a class="section" href="/dashboard/<?php echo $section; ?>"><?php echo $item['label']; ?></a>
+				<ul>
+				<?php foreach ($item['options'] as $option=>$label) : ?>
+					<li class="option<?php if ($section == $this['section'] && $option == $this['option']) echo ' current'; ?>">
+						<a href="/dashboard/<?php echo $section; ?>/<?php echo $option; ?>"<?php if ($option == 'public') echo ' target="_blank"' ?>><?php echo $label; ?></a>
+					</li>
+				<?php endforeach; ?>
+				</ul>
+			</li>
 		<?php endif;?>
     <?php endforeach; ?>
     </ul>
