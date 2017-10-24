38c265fa395495ddd8b8559f0c7b6a5d42174873
H.Yanagimachi <yanagimachi@info-lounge.jp>
2017-10-24 10:47:58 +0000

    Some values changed from env to api

--- view/project/widget/updates.html.php
+++ view/project/widget/updates.html.php
@@ -99,13 +99,7 @@ $level = (int) $this['level'] ?: 3;
 <!-- Lista de entradas -->
 <?php if ($action == 'list') : ?>
     <?php if (!empty($posts)) : ?>
-        <?php while ($post = $pagedResults->fetchPagedRow()) :
-
-                $share_title = $post->title;
-                $share_url = SITE_URL . '/project/'.$project->id.'/updates/' . $post->id;
-                $facebook_url = 'http://facebook.com/sharer.php?u=' . rawurlencode($share_url) . '&t=' . rawurlencode($share_title);
-                $twitter_url = 'http://twitter.com/home?status=' . rawurlencode($share_title . ': ' . $share_url . ' #' . LG_PLACE_LABEL .' @' . LG_TWITTER);
-            ?>
+        <?php while ($post = $pagedResults->fetchPagedRow()) :?>
             <div class="widget post">
                 <?php echo new View('view/blog/post.html.php', array('post' => $post->id, 'show' => 'list', 'url' => '/project/'.$project->id.'/updates/')); ?>
             </div>
