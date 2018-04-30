d7b0a1ab52ba113910a14a3dc2e60405cef373b2
EC2 Default User <ec2-user@staging.localgood.jp>
2018-04-30 09:48:06 +0900

    Remove debug code, show project additional descriptions

--- view/project/widget/share.html.php
+++ view/project/widget/share.html.php
@@ -31,7 +31,6 @@ $share_url = SITE_URL . '/project/' . $project->id;
 $facebook_url = 'http://facebook.com/sharer.php?u=' . urlencode($share_url) . '&t=' . urlencode($share_title);
 $twitter_url = 'http://twitter.com/home?status=' . urlencode($share_title . ': ' . $share_url . ' #' . LG_PLACE_LABEL .' @' . LG_TWITTER);
 
-var_dump($project?true:false);
 ?>
 <script type="text/javascript">
             jQuery(document).ready(function ($) { 
